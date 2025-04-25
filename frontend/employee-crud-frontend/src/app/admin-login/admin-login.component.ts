8c6b756b1792507cb154866a9f081fd20eb67e6b
Shruti Chavan <shchavan04@gmail.com>
2025-04-25 21:48:14 +0530

    Initial commit with frontend and backend

---
+++ frontend/employee-crud-frontend/src/app/admin-login/admin-login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-admin-login',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './admin-login.component.html',
+  styleUrls: ['./admin-login.component.css'],
+  
+})
+export class AdminLoginComponent {
+
+}
+
+
